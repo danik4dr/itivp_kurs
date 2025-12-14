1d54168eed6861252686242ed2ff58c09df4876b
danik4dr <dragundanik4@gmail.com>
2025-12-14 20:28:24 +0300

    Add files via upload

---
+++ my-react-app/tailwind.config.js
@@ -0,0 +1,13 @@
+export default {
+  content: ['./index.html', './src/**/*.{js,jsx}'],
+  theme: {
+    extend: {
+      colors: {
+        darkbg: '#0b1220',
+        accent: '#7c3aed',
+        accent2: '#06b6d4',
+      }
+    }
+  },
+  plugins: []
+}
