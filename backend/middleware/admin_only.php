2bd939e79be5b3d8777497dc9bf9db2c8f4d522e
danik4dr <dragundanik4@gmail.com>
2025-12-14 20:12:27 +0300

    Create admin_only.php

---
+++ backend/middleware/admin_only.php
@@ -0,0 +1,11 @@
+<?php
+
+session_start();
+
+if (!isset($_SESSION['user']) || $_SESSION['user']['role'] !== 'admin') {
+     http_response_code(403);
+     echo json_encode(["success" => false, "error" => "Admin required"]);
+     exit();
+}
+
+return true;
